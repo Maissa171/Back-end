e493774b671a8049bcaedb2788a9a487a9055506
maissa <you@example.com>
2022-05-11 11:50:52 +0100

    first c

---
+++ src/Entity/Consultation.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConsultationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ConsultationRepository::class)
+ */
+class Consultation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $diagnostic;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $note;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Docteur::class, inversedBy="consultation")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $docteur;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Ordonnance::class, mappedBy="consultation")
+     */
+    private $ordonnance;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Patient::class, inversedBy="consultation")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $patient;
+
+    public function __construct()
+    {
+        $this->ordonnance = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDiagnostic(): ?string
+    {
+        return $this->diagnostic;
+    }
+
+    public function setDiagnostic(string $diagnostic): self
+    {
+        $this->diagnostic = $diagnostic;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getDocteur(): ?Docteur
+    {
+        return $this->docteur;
+    }
+
+    public function setDocteur(?Docteur $docteur): self
+    {
+        $this->docteur = $docteur;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ordonnance>
+     */
+    public function getOrdonnance(): Collection
+    {
+        return $this->ordonnance;
+    }
+
+    public function addOrdonnance(Ordonnance $ordonnance): self
+    {
+        if (!$this->ordonnance->contains($ordonnance)) {
+            $this->ordonnance[] = $ordonnance;
+            $ordonnance->setConsultation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrdonnance(Ordonnance $ordonnance): self
+    {
+        if ($this->ordonnance->removeElement($ordonnance)) {
+            // set the owning side to null (unless already changed)
+            if ($ordonnance->getConsultation() === $this) {
+                $ordonnance->setConsultation(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getPatient(): ?Patient
+    {
+        return $this->patient;
+    }
+
+    public function setPatient(?Patient $patient): self
+    {
+        $this->patient = $patient;
+
+        return $this;
+    }
+}
